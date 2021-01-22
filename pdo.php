18674f271c83fbdc42159580d05beccbe3d87082
Dominic Chuma <chumadominic@gmail.com>
2021-01-22 21:40:39 +0100

    pdo connection updated

--- pdo.php
+++ pdo.php
@@ -1,6 +1,7 @@
 <?php
 try{
-    $pdo = new PDO('mysql:host=localhost;port=3306;dbname=game_catalog', 'root', '');
+    // $pdo = new PDO('mysql:host=localhost;port=3306;dbname=game_catalog', 'root', '');
+    $pdo = new PDO('mysql:host=localhost;port=3306;dbname=erlimite_game_play', 'erlimite_demo1', 'deduceme123#');
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Error mode for PDO operations.
 
